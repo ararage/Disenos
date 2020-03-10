3361dddeace2ab922bc326283612d13154142e7e
José Ricardo Pérez <josericardoperez@localhost.localdomain>
2020-03-09 18:37:23 -0600

    Finished Disenos app

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,34 +1,110 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
-
   final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
   final estiloSubtitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Image(
-            image: NetworkImage("https://www.infobae.com/new-resizer/X5vHDo8k-nL0cBStKzzKUaE0juQ=/750x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/07/04170850/Islandia-Cripto.jpg"),
-          ),
-          Row(
-            children: <Widget>[
-              Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _crearImagen(),
+            _crearTitulo(),
+            _crearAcciones(),
+            _crearText(),
+            _crearText(),
+            _crearText(),
+            _crearText(),
+            _crearText()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearImagen() {
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            "https://www.infobae.com/new-resizer/X5vHDo8k-nL0cBStKzzKUaE0juQ=/750x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/07/04170850/Islandia-Cripto.jpg"
+        ),
+        height: 200.0,
+        fit: BoxFit.cover
+      ),
+    );
+  }
+
+  Widget _crearTitulo() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Islandia", style: estiloTitulo,),
-                  Text("La isla de islandia", style: estiloSubtitulo,)
+                  Text(
+                    "Islandia",
+                    style: estiloTitulo,
+                  ),
+                  SizedBox(
+                    height: 7.0,
+                  ),
+                  Text(
+                    "La isla de islandia",
+                    style: estiloSubtitulo,
+                  )
                 ],
               ),
-              Icon(Icons.star, color: Colors.red, size: 30.0),
-              Text('41', style: TextStyle(fontSize: 20.0))
+            ),
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
+      ),
+    );
+  }
 
-            ],
-          )
-        ],
-      )
+  Widget _crearAcciones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _accion(Icons.call, 'CALL'),
+        _accion(Icons.near_me, 'ROUTE'),
+        _accion(Icons.share, 'Share'),
+      ],
+    );
+  }
+
+  Widget _accion(IconData icon, String texto) {
+    return Column(
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Text(texto, style: TextStyle(fontSize: 15.0, color: Colors.blue)),
+      ],
+    );
+  }
+
+  Widget _crearText() {
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Incididunt tempor tempor irure nostrud elit labore culpa excepteur eiusmod excepteur. Elit aute reprehenderit nostrud officia et duis commodo dolor id. Fugiat esse laboris in dolor. Aliquip aute consequat proident consectetur labore et sunt duis. Incididunt et commodo sint nulla.',
+          textAlign: TextAlign.justify,
+        ),
+      ),
     );
   }
 }
